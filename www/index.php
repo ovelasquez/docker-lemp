c3ede869ab2f88b9a08d872c32004899eb9904b0
Mark Oliver <m.oliver@sheffield.ac.uk>
2017-01-18 14:05:53 +0000

    Added links to mysql container and connection check

--- www/index.php
+++ www/index.php
@@ -1,5 +1,34 @@
 <?php
 
-echo 'This is a php test!';
+echo "<p>This is a php/mysql connection test:</p>";
+
+# Fill our vars and run on cli
+# $ php -f db-connect-test.php
+$dbname = 'project';
+$dbuser = 'project';
+$dbpass = 'project';
+$dbhost = 'mysql';
+$connect = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+mysqli_select_db($connect, $dbname) or die("Could not open the db '$dbname'");
+$test_query = "SHOW TABLES FROM $dbname";
+$result = mysqli_query($connect, $test_query);
+$tblCnt = 0;
+while($tbl = mysqli_fetch_array($result)) {
+  $tblCnt++;
+  #echo $tbl[0]."<br />\n";
+}
+
+echo '<p>';
+
+if (!$tblCnt) {
+  echo "There are no tables";
+} else {
+  echo "There are $tblCnt tables";
+}
+
+echo " in the $dbname database.</p>";
+
+# PHP details
+phpinfo();
 
 ?>
